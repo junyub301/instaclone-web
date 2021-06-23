e1f4046060efa59f5090149646fe65cbdeb8c8b7
junyub301 <junyub301@hanmail.net>
2021-06-23 22:44:51 +0900

    introduction to styled-components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./screens/Home";
 import Login from "./screens/Login";
 import NotFound from "./screens/NotFound";
-import { useState } from "react";
 import { useReactiveVar } from "@apollo/client";
 import { isLoggedInVar } from "./apollo";
 function App() {
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,11 +1,37 @@
 import { isLoggedInVar } from "../apollo";
+import styled, { css } from "styled-components";
+import { useState } from "react";
+
+const Title = styled.h1`
+    color: ${(props) => (props.potato ? "palevioletred" : "beige")};
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+        Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+    ${(props) =>
+        props.potato
+            ? css`
+                  font-size: 49px;
+              `
+            : css`
+                  text-decoration: underline;
+              `}
+`;
+
+const Container = styled.div`
+    background-color: tomato;
+`;
+
+const TogglePotato = styled.button`
+    color: red;
+`;
 
 const Login = () => {
+    const [potato, setPotato] = useState(false);
+    const togglePotatao = () => setPotato((current) => !current);
     return (
-        <div>
-            <h1>Login</h1>
-            <button onClick={() => isLoggedInVar(true)}>Log in Now!</button>
-        </div>
+        <Container>
+            <Title potato={potato}>Login</Title>
+            <TogglePotato onClick={togglePotatao}>Toggle Potato</TogglePotato>
+        </Container>
     );
 };
 export default Login;
