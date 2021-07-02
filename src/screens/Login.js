fb0ddb21f3334efe7af597e7b61ee375b7ec2548
junyub301 <junyub301@hanmail.net>
2021-07-02 22:44:12 +0900

    Log in part 1

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -15,6 +15,7 @@ import BottomBox from "../components/auth/BottomBox";
 import PageTitle from "../components/PageTitle";
 import { useForm } from "react-hook-form";
 import FormError from "../components/auth/FormError";
+import { gql, useMutation } from "@apollo/client";
 
 const FacebookLogin = styled.div`
     color: #385285;
@@ -24,14 +25,44 @@ const FacebookLogin = styled.div`
     }
 `;
 
+const LOGIN_MUTATION = gql`
+    mutation login($username: String!, $password: String!) {
+        login(username: $username, password: $password) {
+            ok
+            token
+            error
+        }
+    }
+`;
+
 const Login = () => {
     const {
         register,
         handleSubmit,
         formState: { errors, isValid },
+        getValues,
+        setError,
     } = useForm({ mode: "onChange" });
+
+    const onCompleted = (data) => {
+        const {
+            login: { ok, error, token },
+        } = data;
+        if (!ok) {
+            setError("result", {
+                message: error,
+            });
+        }
+    };
+    const [login, { loading }] = useMutation(LOGIN_MUTATION, { onCompleted });
     const onSubmitValid = (data) => {
-        // console.log(data);
+        if (loading) {
+            return;
+        }
+        const { username, password } = getValues();
+        login({
+            variables: { username, password },
+        });
     };
 
     return (
@@ -68,7 +99,12 @@ const Login = () => {
                         hasError={Boolean(errors?.password?.message)}
                     />
                     <FormError message={errors?.password?.message} />
-                    <Button type='submit' value='Log in' disabled={!isValid} />
+                    <Button
+                        type='submit'
+                        value={loading ? "Loading..." : "Log in"}
+                        disabled={!isValid || loading}
+                    />
+                    <FormError message={errors?.result?.message} />
                 </form>
                 <Separator />
 
