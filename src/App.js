caccacace8d8469e860abe211a2ee23e56cf02d7
junyub301 <junyub301@hanmail.net>
2021-07-02 22:24:07 +0900

    Apollo Client

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./screens/Home";
 import Login from "./screens/Login";
 import NotFound from "./screens/NotFound";
-import { useReactiveVar } from "@apollo/client";
-import { isLoggedInVar, darkModeVar } from "./apollo";
+import { ApolloProvider, useReactiveVar } from "@apollo/client";
+import { isLoggedInVar, darkModeVar, client } from "./apollo";
 import { ThemeProvider } from "styled-components";
 import { darkTheme, lightTheme, GlobalStyles } from "./styles";
 import SignUp from "./screens/SignUp";
@@ -15,27 +15,29 @@ function App() {
     const darkMode = useReactiveVar(darkModeVar);
 
     return (
-        <HelmetProvider>
-            <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
-                <GlobalStyles />
-                <Router>
-                    <Switch>
-                        <Route path={routes.home} exact>
-                            {isLoggedIn ? <Home /> : <Login />}
-                        </Route>
-                        {!isLoggedIn ? (
-                            <Route path={routes.signUp}>
-                                <SignUp />
+        <ApolloProvider client={client}>
+            <HelmetProvider>
+                <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+                    <GlobalStyles />
+                    <Router>
+                        <Switch>
+                            <Route path={routes.home} exact>
+                                {isLoggedIn ? <Home /> : <Login />}
                             </Route>
-                        ) : null}
+                            {!isLoggedIn ? (
+                                <Route path={routes.signUp}>
+                                    <SignUp />
+                                </Route>
+                            ) : null}
 
-                        <Route>
-                            <NotFound />
-                        </Route>
-                    </Switch>
-                </Router>
-            </ThemeProvider>
-        </HelmetProvider>
+                            <Route>
+                                <NotFound />
+                            </Route>
+                        </Switch>
+                    </Router>
+                </ThemeProvider>
+            </HelmetProvider>
+        </ApolloProvider>
     );
 }
 
