8c9d5162cf901fe3742a5bf843fbb01412b4d584
junyub301 <junyub301@hanmail.net>
2021-07-08 23:05:38 +0900

    Comments part 1

--- src/components/feed/Photo.js
+++ src/components/feed/Photo.js
@@ -68,7 +68,32 @@ const Likes = styled(FatText)`
     display: block;
 `;
 
-function Photo({ id, user, file, isLiked, likes }) {
+const Comments = styled.div`
+    margin-top: 20px;
+`;
+const Comment = styled.div``;
+const CommentCaption = styled.span`
+    margin-left: 10px;
+`;
+
+const CommentCount = styled.span`
+    opacity: 0.7;
+    margin: 10px 0px;
+    display: block;
+    font-weight: 600;
+    font-size: 12px;
+`;
+
+function Photo({
+    id,
+    user,
+    file,
+    isLiked,
+    likes,
+    caption,
+    commentNumber,
+    comments,
+}) {
     const updateToggleLike = (cache, result) => {
         const {
             data: {
@@ -141,6 +166,17 @@ function Photo({ id, user, file, isLiked, likes }) {
                     </div>
                 </PhotoActions>
                 <Likes>{likes === 1 ? "1 like" : `${likes} likes`}</Likes>
+                <Comments>
+                    <Comment>
+                        <FatText>{user.username}</FatText>
+                        <CommentCaption>{caption}</CommentCaption>
+                    </Comment>
+                    <CommentCount>
+                        {commentNumber === 1
+                            ? "1 comment"
+                            : `${commentNumber} comments`}
+                    </CommentCount>
+                </Comments>
             </PhotoData>
         </PhotoContainer>
     );
@@ -152,9 +188,12 @@ Photo.protoTypes = {
         avatar: PropTypes.string,
         username: PropTypes.string.isRequired,
     }),
+    caption: PropTypes.string,
     file: PropTypes.string.isRequired,
     isLiked: PropTypes.bool.isRequired,
     likes: PropTypes.number.isRequired,
+    commentNumber: PropTypes.number.isRequired,
+    comments: PropTypes.arrayOf(PropTypes.shape({})),
 };
 
 export default Photo;
