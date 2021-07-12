37527541b2b5932ff7a35b975b32e7b12144a4ae
junyub301 <junyub0301@gmail.com>
2021-07-12 23:02:45 +0900

    Parsing Hashtags

--- src/components/feed/Comment.js
+++ src/components/feed/Comment.js
@@ -1,17 +1,37 @@
 import styled from "styled-components";
 import { FatText } from "../shared";
 import PropTypes from "prop-types";
+import * as sanitizeHtml from "sanitize-html";
 
 const CommentContainer = styled.div``;
 const CommentCaption = styled.span`
     margin-left: 10px;
+    mark {
+        background-color: inherit;
+        color: ${(props) => props.theme.accent};
+        cursor: pointer;
+        &:hover {
+            text-decoration: underline;
+        }
+    }
 `;
 
 function Comment({ author, payload }) {
+    const cleanPayload = sanitizeHtml(
+        payload.replace(/#[\w]+/g, "<mark>$&</mark>"),
+        {
+            allowedTags: ["mark"],
+        }
+    );
+
     return (
         <CommentContainer>
             <FatText>{author}</FatText>
-            <CommentCaption>{payload}</CommentCaption>
+            <CommentCaption
+                dangerouslySetInnerHTML={{
+                    __html: cleanPayload,
+                }}
+            />
         </CommentContainer>
     );
 }
