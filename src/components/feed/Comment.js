25771d18656389060808b83c029f0848c694d71c
junyub301 <junyub0301@gmail.com>
2021-07-12 23:19:49 +0900

    Parsing Hashtags part 2

--- src/components/feed/Comment.js
+++ src/components/feed/Comment.js
@@ -1,12 +1,13 @@
+import React from "react";
 import styled from "styled-components";
 import { FatText } from "../shared";
 import PropTypes from "prop-types";
-import * as sanitizeHtml from "sanitize-html";
+import { Link } from "react-router-dom";
 
 const CommentContainer = styled.div``;
 const CommentCaption = styled.span`
     margin-left: 10px;
-    mark {
+    a {
         background-color: inherit;
         color: ${(props) => props.theme.accent};
         cursor: pointer;
@@ -17,21 +18,20 @@ const CommentCaption = styled.span`
 `;
 
 function Comment({ author, payload }) {
-    const cleanPayload = sanitizeHtml(
-        payload.replace(/#[\w]+/g, "<mark>$&</mark>"),
-        {
-            allowedTags: ["mark"],
-        }
-    );
-
     return (
         <CommentContainer>
             <FatText>{author}</FatText>
-            <CommentCaption
-                dangerouslySetInnerHTML={{
-                    __html: cleanPayload,
-                }}
-            />
+            <CommentCaption>
+                {payload.split(" ").map((word, index) =>
+                    /#[\w]+/.test(word) ? (
+                        <React.Fragment key={index}>
+                            <Link to={`/hashtags/${word}`}>{word}</Link>{" "}
+                        </React.Fragment>
+                    ) : (
+                        <React.Fragment key={index}>{word} </React.Fragment>
+                    )
+                )}
+            </CommentCaption>
         </CommentContainer>
     );
 }
