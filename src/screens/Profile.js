af28d447d00c34fa6723c8ffb99c6d8d78b75f12
junyub301 <junyub0301@gmail.com>
2021-07-15 23:11:39 +0900

    Follow Unfollow part 1

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -5,6 +5,24 @@ import { useParams } from "react-router-dom";
 import { PHOTO_FRAGMENT } from "../fragments";
 import styled from "styled-components";
 import { FatText } from "../components/shared";
+import Button from "../components/auth/Button";
+import PageTitle from "../components/PageTitle";
+
+const FOLLOW_USER_MUTATION = gql`
+    mutation followUser($username: String!) {
+        followUser(username: $username) {
+            ok
+        }
+    }
+`;
+
+const UNFOLLOW_USER_MUTATION = gql`
+    mutation unfollowUser($username: String!) {
+        unfollowUser(username: $username) {
+            ok
+        }
+    }
+`;
 
 const SEE_PROFILE_QUERY = gql`
     query seeProfile($username: String!) {
@@ -45,6 +63,7 @@ const Username = styled.h3`
 const Row = styled.div`
     margin-bottom: 20px;
     font-size: 16px;
+    display: flex;
 `;
 const List = styled.ul`
     display: flex;
@@ -99,20 +118,47 @@ const Icon = styled.span`
     }
 `;
 
+// 상속된 스타일은 유지하되 태그 타입을 변형 시킬 때 사용
+const ProfileBtn = styled(Button).attrs({
+    as: "span",
+})`
+    margin-left: 10px;
+    margin-top: 0px;
+`;
+
 function Profile() {
     const { username } = useParams();
-    const { data } = useQuery(SEE_PROFILE_QUERY, {
+    const { data, loading } = useQuery(SEE_PROFILE_QUERY, {
         variables: {
             username,
         },
     });
+    const getButton = (seeProfile) => {
+        const { isMe, isFollwoing } = seeProfile;
+        if (isMe) {
+            return <ProfileBtn>Edit Profile</ProfileBtn>;
+        }
+        if (isFollwoing) {
+            return <ProfileBtn>Unfollow</ProfileBtn>;
+        } else {
+            return <ProfileBtn>Follow</ProfileBtn>;
+        }
+    };
     return (
         <div>
+            <PageTitle
+                title={
+                    loading
+                        ? "Loading..."
+                        : `${data?.seeProfile?.username}'s Profile`
+                }
+            />
             <Header>
                 <Avatar src={data?.seeProfile?.avatar} />
                 <Column>
                     <Row>
                         <Username>{data?.seeProfile?.username}</Username>
+                        {data?.seeProfile ? getButton(data.seeProfile) : null}
                     </Row>
                     <Row>
                         <List>
@@ -146,7 +192,7 @@ function Profile() {
             </Header>
             <Grid>
                 {data?.seeProfile?.photos.map((photo) => (
-                    <Photo bg={photo.file}>
+                    <Photo key={photo.id} bg={photo.file}>
                         <Icons>
                             <Icon>
                                 <FontAwesomeIcon icon={faHeart} />
